codekandis/tiphy-session-integration

Adds configuration registry support.

src/Configurations/ConfigurationRegistryInterface.php
@@ -1,0 +1,19 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TiphySessionIntegration\Configurations;
+
+use CodeKandis\Configurations\ConfigurationRegistryInterface as OriginConfigurationRegistryInterface;
+use CodeKandis\Session\Configurations\SessionsConfigurationInterface;
+
+/**
+ * Represents the interface of any configuration registry providing a sessions configuration.
+ * @package codekandis/tiphy-sessions-integration
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface ConfigurationRegistryInterface extends OriginConfigurationRegistryInterface
+{
+	/**
+	 * Gets the sessions configuration.
+	 * @return ?SessionsConfigurationInterface The sessions configuration.
+	 */
+	public function getSessionsConfiguration(): ?SessionsConfigurationInterface;
+}

src/Configurations/ConfigurationRegistryTrait.php
@@ -1,0 +1,26 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TiphySessionIntegration\Configurations;
+
+use CodeKandis\Session\Configurations\SessionsConfigurationInterface;
+
+/**
+ * Represents the trait to integrate a sessions configuration into a configuration registry.
+ * @package codekandis/tiphy-sessions-integration
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+trait ConfigurationRegistryTrait
+{
+	/**
+	 * Stores the sessions configuration.
+	 * @var ?SessionsConfigurationInterface
+	 */
+	private ?SessionsConfigurationInterface $sessionsConfiguration = null;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getSessionsConfiguration(): ?SessionsConfigurationInterface
+	{
+		return $this->sessionsConfiguration;
+	}
+}

codekandis/tiphy-sessions-integration

src/Configurations/ConfigurationRegistryInterface.php
@@ -1,0 +1,19 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TiphySessionsIntegration\Configurations;
+
+use CodeKandis\Configurations\ConfigurationRegistryInterface as OriginConfigurationRegistryInterface;
+use CodeKandis\Sessions\Configurations\SessionsConfigurationInterface;
+
+/**
+ * Represents the interface of any configuration registry providing a sessions configuration.
+ * @package codekandis/tiphy-sessions-integration
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+interface ConfigurationRegistryInterface extends OriginConfigurationRegistryInterface
+{
+	/**
+	 * Gets the sessions configuration.
+	 * @return ?SessionsConfigurationInterface The sessions configuration.
+	 */
+	public function getSessionsConfiguration(): ?SessionsConfigurationInterface;
+}

src/Configurations/ConfigurationRegistryTrait.php
@@ -1,0 +1,26 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TiphySessionsIntegration\Configurations;
+
+use CodeKandis\Sessions\Configurations\SessionsConfigurationInterface;
+
+/**
+ * Represents the trait to integrate a sessions configuration into a configuration registry.
+ * @package codekandis/tiphy-sessions-integration
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+trait ConfigurationRegistryTrait
+{
+	/**
+	 * Stores the sessions configuration.
+	 * @var ?SessionsConfigurationInterface
+	 */
+	private ?SessionsConfigurationInterface $sessionsConfiguration = null;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getSessionsConfiguration(): ?SessionsConfigurationInterface
+	{
+		return $this->sessionsConfiguration;
+	}
+}