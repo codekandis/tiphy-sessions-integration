codekandis/tiphy-session-integration

Adds the sessions configuration.

src/Configurations/SessionsConfiguration.php
@@ -1,0 +1,29 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TiphySessionIntegration\Configurations;
+
+use CodeKandis\Configurations\AbstractConfiguration;
+use CodeKandis\Session\Configurations\SessionsConfigurationInterface;
+
+/**
+ * Represents a sessions configuration.
+ * @package codekandis/tiphy-sessions-integration
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionsConfiguration extends AbstractConfiguration implements SessionsConfigurationInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getOptions(): array
+	{
+		return $this->read( 'options' );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getSavePath(): ?string
+	{
+		return $this->readOrDefault( 'savePath', null );
+	}
+}

codekandis/tiphy-sessions-integration

src/Configurations/SessionsConfiguration.php
@@ -1,0 +1,29 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TiphySessionsIntegration\Configurations;
+
+use CodeKandis\Configurations\AbstractConfiguration;
+use CodeKandis\Sessions\Configurations\SessionsConfigurationInterface;
+
+/**
+ * Represents a sessions configuration.
+ * @package codekandis/tiphy-sessions-integration
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionsConfiguration extends AbstractConfiguration implements SessionsConfigurationInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getOptions(): array
+	{
+		return $this->read( 'options' );
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getSavePath(): ?string
+	{
+		return $this->readOrDefault( 'savePath', null );
+	}
+}